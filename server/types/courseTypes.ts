b930b42bdb01b57c6333db33d39103ae3168a084
AbdelrahmanNasser77 <sim.abdelrahmanmohamed36283@alexu.edu.eg>
2025-08-10 02:19:39 +0300

    Add outcomes and prerequisites fields to ICourseInfo interface

--- server/types/courseTypes.ts
+++ server/types/courseTypes.ts
@@ -3,6 +3,8 @@ import mongoose, { Document } from "mongoose";
 export interface ICourseInfo {
   title: string;
   description: string;
+  outcomes: string[];
+  prerequisites: string[]; // string array of prerequisite course IDs
 }
 
 export interface ICourse extends Document, ICourseInfo {}
